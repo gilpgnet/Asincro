7091a8e9b1c2e54fe260f00a7a1be8263215e039
GIlberto Pacheco Gallegos <gilpgnet@gmail.com>
2019-02-03 18:11:58 -0600

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -1,5 +1,4 @@
 @echo off
-PATH=C:\Program Files\Java\jdk1.8.0_191\bin;%PATH%
 cd /d %0\..
 call gradlew clean
 del logcat.txt
@@ -7,4 +6,3 @@ del gradlew
 del gradlew.bat
 rmdir /S /Q .gradle
 rmdir /S /Q gradle
-pause
